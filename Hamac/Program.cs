d9ea2136c62c65ca8dedb846bc98e54045312187
Cristiano Cunha <contato@cristianoprogramador.com>
2022-04-21 09:50:53 -0300

    Adiciona exemplo de HmacSha256

---
+++ Hamac/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+Console.Write("Digite a entrada: ");
+var text = Console.ReadLine();
+Console.Write("Digite a senha: ");
+var password = Console.ReadLine();
+
+var hmacSha256 =  new HMACSHA256(Encoding.UTF8.GetBytes(password));
+var hmac = hmacSha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+string stringByteHmac = BitConverter.ToString(hmac).Replace("-", string.Empty);
+Console.WriteLine(stringByteHmac);
