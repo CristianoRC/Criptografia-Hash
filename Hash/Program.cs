323ef1d7e58911a9d389f9b99f35f39b254bdec1
Cristiano Cunha <contato@cristianoprogramador.com>
2022-04-21 09:42:51 -0300

    Cria exemplo de hash

---
+++ Hash/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+Console.Write("Digite a entrada: ");
+var text = Console.ReadLine();
+
+using var sha256 = SHA256.Create();
+var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+string stringByte = BitConverter.ToString(hash).Replace("-", string.Empty);
+Console.WriteLine(stringByte);
